bcba34b5156ac2a6790240ae2700b2c4dc623c78
dentra <dennis.trachuk@gmail.com>
2021-10-23 22:34:11 +0000

    miot: fix hexstr

--- components/miot/miot_utils.cpp
+++ components/miot/miot_utils.cpp
@@ -7,9 +7,10 @@ constexpr char hexmap[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A'
 
 std::string hexstr(const uint8_t *data, uint32_t size) {
   std::string str;
+  str.resize(size * 2);
   for (int i = 0; i < size; ++i) {
     str[2 * i + 0] = hexmap[(data[i] & 0xF0) >> 4];
-    str[2 * i + 1] = hexmap[data[i] & 0x0F];
+    str[2 * i + 1] = hexmap[(data[i] & 0x0F) >> 0];
   }
   return str;
 }
