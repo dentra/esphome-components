8453a7ae4cd92aab2b60b663238bc874238ed7da
dentra <dennis.trachuk@gmail.com>
2022-09-28 17:32:55 +0000

    update esphome version and post_create script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,22 +2,16 @@
 
 set -e
 
-pio_ini=platformio.ini
+sedi() {
+  expr=$1
+  file=$2
+  # Failed on VirtioFS, so workaraund it
+  # sed -i "$expr" "$file"
+  temp=$(mktemp)
+  sed "$expr" "$file" > $temp && mv $temp $file
+}
 
-files=($pio_ini .clang-format .clang-tidy .editorconfig)
-
-for f in "${files[@]}" ; do
-  if [ ! -f "$f" ]; then
-    curl -Ls "https://github.com/esphome/esphome/raw/dev/$f" -o "$f"
-  fi
-done
-
-
-# replace "esphome" to "." in src_dir. esphome linked at post-start.sh
-sed -i -e "/src_dir/s/esphome/\./" $pio_ini
-# replace ".temp" to ".esphome in "sdkconfig_path"
-sed -i -e "/sdkconfig_path/s/.temp/.esphome/" $pio_ini
-
+# take ownership of mounted folders
 files=(.esphome .pio)
 for f in "${files[@]}" ; do
   if [ ! -d "$f" ]; then
@@ -27,20 +21,41 @@ for f in "${files[@]}" ; do
   fi
 done
 
+pio_ini=platformio.ini
+
+files=($pio_ini .clang-format .clang-tidy .editorconfig .pylintrc)
+for f in "${files[@]}" ; do
+  if [ ! -f "$f" ] || [ "$f" = "$pio_ini" ]; then
+    curl -Ls "https://github.com/esphome/esphome/raw/dev/$f" -o ".esphome/$f"
+    ln -svf ".esphome/$f" "$f"
+  fi
+done
+
+# replace "esphome" to "." in src_dir. esphome linked at post-start.sh
+sedi "/src_dir/s/esphome/\./" ".esphome/$pio_ini"
+# replace ".temp" to ".esphome in "sdkconfig_path"
+sedi "/sdkconfig_path/s/.temp/.esphome/" ".esphome/$pio_ini"
+
+# add project specific defines
+# defines=()
+# for d in "${defines[@]}" ; do
+#   defines_r="$defines_r\n    -D$d"
+# done
+# sedi "s/ESPHOME_LOG_LEVEL_VERY_VERBOSE/ESPHOME_LOG_LEVEL_VERY_VERBOSE$defines_r/" ".esphome/$pio_ini"
+
 cpp_json=.vscode/c_cpp_properties.json
-if [ ! -f $cpp_json ]; then
-    pio init --ide vscode --silent
-    sed -i "/\\/workspaces\/esphome\/include/d" $cpp_json
-    rm CMakeLists.txt components/CMakeLists.txt
+#if [ ! -f $cpp_json ]; then
+if [ "$(find /esphome/.temp/platformio/* -maxdepth 0 -type d 2>/dev/null|wc -l)" -lt "1" ]; then
+    pio init --ide vscode --silent -e esp32-arduino
+    sedi "/\\/workspaces\/esphome\/include/d" $cpp_json
+    rm -rf CMakeLists.txt components/CMakeLists.txt
 else
     echo "Cpp environment already configured. To reconfigure it you could run one the following commands:"
-    echo "  pio init --ide vscode -e esp8266"
-    echo "  pio init --ide vscode -e esp32"
+    echo "  pio init --ide vscode -e esp8266-arduino"
+    echo "  pio init --ide vscode -e esp32-arduino"
     echo "  pio init --ide vscode -e esp32-idf"
 fi
 
 # additionally remove annoying pio ide recomendations
-sed -i "/platformio.platformio-ide/d" .vscode/extensions.json
+sedi "/platformio.platformio-ide/d" .vscode/extensions.json
 
-sudo chown vscode lib/.esphome
-sudo chown vscode lib/miot/.esphome
