bf41a2fa22dafbc3b2d924c6518e181bfab8fefe
dentra <dennis.trachuk@gmail.com>
2023-06-29 17:01:34 +0000

    miot_toothbrush: update score only at end event

--- components/miot_toothbrush/miot_toothbrush.cpp
+++ components/miot_toothbrush/miot_toothbrush.cpp
@@ -15,7 +15,9 @@ void MiotToothbrush::dump_config() {
 void MiotToothbrush::process_toothbrush_event_(const miot::BLEObject &obj) {
   const auto toothbrush_event = obj.get_toothbrush_event();
   if (toothbrush_event.has_value()) {
-    this->publish_state(toothbrush_event->score);
+    if (toothbrush_event->type != miot::ToothbrushEvent::BRUSHING_START) {
+      this->publish_state(toothbrush_event->score);
+    }
   }
 }
 
