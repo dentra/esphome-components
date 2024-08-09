ff8db73b9447c963cf1d5b86b09f471a8c0b5e97
dentra <dennis.trachuk@gmail.com>
2024-08-09 17:42:35 +0300

    nvs: add blob support

--- components/nvs/nvs.cpp
+++ components/nvs/nvs.cpp
@@ -16,6 +16,28 @@ bool NvsFlash::open(const char *namespace_name, nvs_open_mode_t open_mode) {
   return false;
 }
 
+bool NvsFlash::commit() const {
+  if (this->handle_) {
+    auto err = this->handle_->commit();
+    if (err == ESP_OK) {
+      return true;
+    }
+    NVS_TRACE(TAG, "commit failed: %s", esp_err_to_name(err));
+  }
+  return false;
+}
+
+bool NvsFlash::erase(const char *key) const {
+  if (this->handle_) {
+    auto err = this->handle_->erase_item(key);
+    if (err == ESP_OK) {
+      return true;
+    }
+    NVS_TRACE(TAG, "erase %s failed: %s", key, esp_err_to_name(err));
+  }
+  return false;
+}
+
 optional<std::string> NvsFlash::get_string_(const char *key) const {
   if (!this->handle_) {
     return {};
@@ -24,7 +46,7 @@ optional<std::string> NvsFlash::get_string_(const char *key) const {
   size_t len{};
   esp_err_t err = this->handle_->get_item_size(nvs::ItemType::SZ, key, len);
   if (err != ESP_OK) {
-    ESP_LOGV(TAG, "get_item_size %s: %s - the key might not be set yet", key, esp_err_to_name(err));
+    NVS_TRACE(TAG, "get_item_size %s: %s - the key might not be set yet", key, esp_err_to_name(err));
     return {};
   }
 
@@ -32,35 +54,159 @@ optional<std::string> NvsFlash::get_string_(const char *key) const {
   res.resize(len - 1);
   err = this->handle_->get_string(key, res.data(), len);
   if (err != ESP_OK) {
-    ESP_LOGV(TAG, "get string %s failed: %s", key, esp_err_to_name(err));
+    NVS_TRACE(TAG, "get string %s failed: %s", key, esp_err_to_name(err));
     return {};
   }
   NVS_TRACE(TAG, "get string %s success: %s", key, res.c_str());
   return res;
 }
 
-optional<std::vector<uint8_t>> NvsFlash::get_blob_(const char *key) const {
+bool NvsFlash::set_string_(const char *key, const char *value) const {
+  if (!this->handle_) {
+    return false;
+  }
+  auto err = this->handle_->set_string(key, value);
+  if (err != ESP_OK) {
+    NVS_TRACE(TAG, "set string %s failed: %s", key, esp_err_to_name(err));
+    return false;
+  }
+  NVS_TRACE(TAG, "set string %s success: %s", key, value);
+  return true;
+}
+
+size_t NvsFlash::get_blob(const char *key, uint8_t *data, size_t size) const {
+  if (!this->handle_) {
+    return false;
+  }
+
+  if (data == nullptr) {
+    esp_err_t err = this->handle_->get_item_size(nvs::ItemType::BLOB, key, size);
+    if (err != ESP_OK) {
+      NVS_TRACE(TAG, "get_item_size %s: %s - the key might not be set yet", key, esp_err_to_name(err));
+      return {};
+    }
+  }
+
+  esp_err_t err = this->handle_->get_blob(key, data, size);
+  if (err != ESP_OK) {
+    NVS_TRACE(TAG, "get blob %s failed: %s", key, esp_err_to_name(err));
+    return false;
+  }
+  NVS_TRACE(TAG, "get blob %s success: %s", key, format_hex_pretty(data, size).c_str());
+  return size;
+}
+
+optional<std::vector<uint8_t>> NvsFlash::get_blob(const char *key) const {
+  size_t size = this->get_blob(key, nullptr, 0);
+  if (size != 0) {
+    std::vector<uint8_t> res(size);
+    if (this->get_blob(key, res.data(), res.size())) {
+      return {res};
+    }
+  }
+  return {};
+}
+
+bool NvsFlash::set_blob_(const char *key, const void *value, size_t size) const {
+  if (!this->handle_) {
+    return false;
+  }
+  auto err = this->handle_->set_blob(key, value, size);
+  if (err != ESP_OK) {
+    NVS_TRACE(TAG, "set blob %s failed: %s", key, esp_err_to_name(err));
+    return false;
+  }
+  NVS_TRACE(TAG, "set blob %s success: %s", key, format_hex_pretty(static_cast<const uint8_t *>(value), size).c_str());
+  return true;
+}
+
+optional<float> NvsFlash::get_float_(const char *key) const {
   if (!this->handle_) {
     return {};
   }
-
-  size_t len{};
-  esp_err_t err = this->handle_->get_item_size(nvs::ItemType::BLOB, key, len);
+  union {
+    uint32_t u;
+    float f;
+  } res{};
+  auto err = this->handle_->get_item(key, res.u);
   if (err != ESP_OK) {
-    ESP_LOGV(TAG, "get_item_size %s: %s - the key might not be set yet", key, esp_err_to_name(err));
+    NVS_TRACE(TAG, "get float %s failed: %s", key, esp_err_to_name(err));
     return {};
   }
+  NVS_TRACE(TAG, "get float %s success: %f", key, res.f);
+  return {res.f};
+}
 
-  std::vector<uint8_t> res;
-  res.resize(len);
-  err = this->handle_->get_blob(key, res.data(), len);
+bool NvsFlash::set_float_(const char *key, float value) const {
+  static_assert(sizeof(float) == sizeof(uint32_t));
+  if (!this->handle_) {
+    return false;
+  }
+  const auto *cv = static_cast<const uint32_t *>(static_cast<const void *>(&value));
+  auto err = this->handle_->set_item(key, *cv);
   if (err != ESP_OK) {
-    ESP_LOGV(TAG, "get blob %s failed: %s", key, esp_err_to_name(err));
+    NVS_TRACE(TAG, "set float %s failed: %s", key, esp_err_to_name(err));
+    return false;
+  }
+  NVS_TRACE(TAG, "set float %s success: %f", key, value);
+  return true;
+}
+
+optional<double> NvsFlash::get_double_(const char *key) const {
+  union {
+    uint64_t u;
+    double d;
+  } res{};
+  auto err = this->handle_->get_item(key, res.u);
+  if (err != ESP_OK) {
+    NVS_TRACE(TAG, "get double %s failed: %s", key, esp_err_to_name(err));
     return {};
   }
-  res.resize(len);
-  NVS_TRACE(TAG, "get blob %s success: %s", key, format_hex_pretty(res).c_str());
-  return res;
+  NVS_TRACE(TAG, "get double %s success: %f", key, res.d);
+  return {res.d};
+}
+
+bool NvsFlash::set_double_(const char *key, double value) const {
+  static_assert(sizeof(double) == sizeof(uint64_t));
+  if (!this->handle_) {
+    return false;
+  }
+  const auto *cv = static_cast<const uint64_t *>(static_cast<const void *>(&value));
+  auto err = this->handle_->set_item(key, *cv);
+  if (err != ESP_OK) {
+    NVS_TRACE(TAG, "set double %s failed: %s", key, esp_err_to_name(err));
+    return false;
+  }
+  NVS_TRACE(TAG, "set double %s success: %f", key, value);
+  return true;
+}
+
+optional<bool> NvsFlash::get_bool_(const char *key) const {
+  if (!this->handle_) {
+    return {};
+  }
+  uint8_t res{};
+  auto err = this->handle_->get_item(key, res);
+  if (err != ESP_OK) {
+    NVS_TRACE(TAG, "get bool %s failed: %s", key, esp_err_to_name(err));
+    return {};
+  }
+  NVS_TRACE(TAG, "get bool %s success: %s", key, TRUEFALSE(res != 0));
+  return {res != 0};
+}
+
+bool NvsFlash::set_bool_(const char *key, bool value) const {
+  if (!this->handle_) {
+    return false;
+  }
+  const uint8_t cv = value ? 1 : 0;
+  auto err = this->handle_->set_item(key, cv);
+  if (err != ESP_OK) {
+    NVS_TRACE(TAG, "set bool %s failed: %s", key, esp_err_to_name(err));
+    return false;
+  }
+  NVS_TRACE(TAG, "set bool %s success: %s", key, TRUEFALSE(value));
+  return true;
 }
 
 }  // namespace nvs_flash
--- components/nvs/nvs.h
+++ components/nvs/nvs.h
@@ -19,7 +19,7 @@
 namespace esphome {
 namespace nvs_flash {
 
-#define NVS_TRACE ESP_LOGD
+#define NVS_TRACE ESP_LOGV
 
 template<typename T>  // NOLINTNEXTLINE(readability-identifier-naming)
 struct is_nvs_type : std::integral_constant<bool, std::is_integral<T>::value && !std::is_same<T, bool>::value> {};
@@ -46,91 +46,55 @@ class NvsFlash {
 
   void close() {
     if (this->handle_) {
+      // The handle is automatically closed on destruction.
       this->handle_.reset();
     }
   }
 
-  bool commit() const {
-    if (this->handle_) {
-      auto err = this->handle_->commit();
-      if (err == ESP_OK) {
-        return true;
-      }
-      NVS_TRACE("nvs", "commit failed: %s", esp_err_to_name(err));
-    }
-    return false;
-  }
+  bool commit() const;
 
-  bool erase(const char *key) const {
-    if (this->handle_) {
-      auto err = this->handle_->erase_item(key);
-      if (err == ESP_OK) {
-        return true;
-      }
-      NVS_TRACE("nvs", "erase %s failed: %s", key, esp_err_to_name(err));
-    }
-    return false;
-  }
-
+  bool erase(const char *key) const;
   bool erase(const std::string &key) const { return this->erase(key.c_str()); }
 
   template<typename T, enable_if_t<std::is_same<T, std::string>::value, int> = 0>
   optional<T> get(const char *key) const {
-    if (!this->handle_) {
-      return {};
-    }
-    return {this->get_string_(key)};
+    return this->get_string_(key);
+  }
+  template<typename T, enable_if_t<std::is_same<T, std::string>::value, int> = 0>
+  optional<T> get(const std::string &key) const {
+    return this->get_string_(key.c_str());
   }
 
   template<typename T, enable_if_t<std::is_same<T, float>::value, int> = 0> optional<T> get(const char *key) const {
     static_assert(sizeof(float) == sizeof(uint32_t));
-    if (!this->handle_) {
-      return {};
-    }
-    union {
-      uint32_t u;
-      float f;
-    } res{};
-    auto err = this->handle_->get_item(key, res.u);
-    if (err != ESP_OK) {
-      NVS_TRACE("nvs", "get float %s failed: %s", key, esp_err_to_name(err));
-      return {};
-    }
-    NVS_TRACE("nvs", "get float %s success: %f", key, res.f);
-    return {res.f};
+    return this->get_float_(key);
+  }
+  template<typename T, enable_if_t<std::is_same<T, float>::value, int> = 0>
+  optional<T> get(const std::string &key) const {
+    static_assert(sizeof(float) == sizeof(uint32_t));
+    return this->get_float_(key.c_str());
   }
 
   template<typename T, enable_if_t<std::is_same<T, double>::value, int> = 0> optional<T> get(const char *key) const {
     static_assert(sizeof(double) == sizeof(uint64_t));
-    union {
-      uint64_t u;
-      double d;
-    } res{};
-    auto err = this->handle_->get_item(key, res.u);
-    if (err != ESP_OK) {
-      NVS_TRACE("nvs", "get double %s failed: %s", key, esp_err_to_name(err));
-      return {};
-    }
-    NVS_TRACE("nvs", "get double %s success: %f", key, res.d);
-    return {res.d};
+    return this->get_double_(key);
+  }
+  template<typename T, enable_if_t<std::is_same<T, double>::value, int> = 0>
+  optional<T> get(const std::string &key) const {
+    static_assert(sizeof(double) == sizeof(uint64_t));
+    return this->get_double_(key.c_str());
   }
 
   template<typename T, enable_if_t<std::is_same<T, bool>::value, int> = 0> optional<T> get(const char *key) const {
-    if (!this->handle_) {
-      return {};
-    }
-    uint8_t res{};
-    auto err = this->handle_->get_item(key, res);
-    if (err != ESP_OK) {
-      NVS_TRACE("nvs", "get bool %s failed: %s", key, esp_err_to_name(err));
-      return {};
-    }
-    NVS_TRACE("nvs", "get bool %s success: %s", key, TRUEFALSE(res != 0));
-    return {res != 0};
+    return this->get_bool_(key);
+  }
+  template<typename T, enable_if_t<std::is_same<T, bool>::value, int> = 0>
+  optional<T> get(const std::string &key) const {
+    return this->get_bool_(key.c_str());
   }
 
   template<typename T, enable_if_t<is_nvs_type<T>::value, int> = 0> optional<T> get(const char *key) const {
-    static_assert(!std::is_same<T, bool>::value);
+    static_assert(!std::is_same<T, bool>::value);  // check that is not bool
     if (!this->handle_) {
       return {};
     }
@@ -145,7 +109,7 @@ class NvsFlash {
   }
 
   template<typename T, enable_if_t<is_nvs_type<T>::value, int> = 0> bool set(const char *key, T value) const {
-    static_assert(!std::is_same<T, bool>::value);
+    static_assert(!std::is_same<T, bool>::value);  // check that is not bool
     if (!this->handle_) {
       return false;
     }
@@ -158,95 +122,62 @@ class NvsFlash {
     return true;
   }
 
-  bool set(const char *key, bool value) const {
-    if (!this->handle_) {
-      return false;
-    }
-    const uint8_t cv = value ? 1 : 0;
-    auto err = this->handle_->set_item(key, cv);
-    if (err != ESP_OK) {
-      NVS_TRACE("nvs", "set bool %s failed: %s", key, esp_err_to_name(err));
-      return false;
-    }
-    NVS_TRACE("nvs", "set bool %s success: %s", key, TRUEFALSE(value));
-    return true;
-  }
+  bool set(const char *key, bool value) const { return this->set_bool_(key, value); }
+  bool set(const std::string &key, bool value) const { return this->set(key.c_str(), value); }
 
-  bool set(const char *key, float value) const {
-    static_assert(sizeof(float) == sizeof(uint32_t));
-    if (!this->handle_) {
-      return false;
-    }
-    const auto *cv = static_cast<const uint32_t *>(static_cast<const void *>(&value));
-    auto err = this->handle_->set_item(key, *cv);
-    if (err != ESP_OK) {
-      NVS_TRACE("nvs", "set float %s failed: %s", key, esp_err_to_name(err));
-      return false;
-    }
-    NVS_TRACE("nvs", "set float %s success: %f", key, value);
-    return true;
-  }
+  bool set(const char *key, float value) const { return this->set_float_(key, value); }
+  bool set(const std::string &key, float value) const { return this->set(key.c_str(), value); }
 
-  bool set(const char *key, double value) const {
-    static_assert(sizeof(double) == sizeof(uint64_t));
-    if (!this->handle_) {
-      return false;
-    }
-    const auto *cv = static_cast<const uint64_t *>(static_cast<const void *>(&value));
-    auto err = this->handle_->set_item(key, *cv);
-    if (err != ESP_OK) {
-      NVS_TRACE("nvs", "set double %s failed: %s", key, esp_err_to_name(err));
-      return false;
-    }
-    NVS_TRACE("nvs", "set double %s success: %f", key, value);
-    return true;
-  }
+  bool set(const char *key, double value) const { return this->set_double_(key, value); }
+  bool set(const std::string &key, double value) const { return this->set(key.c_str(), value); }
 
-  bool set(const char *key, const char *value) const {
-    if (!this->handle_) {
-      return false;
-    }
-    auto err = this->handle_->set_string(key, value);
-    if (err != ESP_OK) {
-      NVS_TRACE("nvs", "set string %s failed: %s", key, esp_err_to_name(err));
-      return false;
-    }
-    NVS_TRACE("nvs", "set string %s success: %s", key, value);
-    return true;
-  }
-
+  bool set(const char *key, const char *value) const { return this->set_string_(key, value); }
+  bool set(const std::string &key, const char *value) const { return this->set(key.c_str(), value); }
   bool set(const char *key, const std::string &value) const { return this->set(key, value.c_str()); }
+  bool set(const std::string &key, const std::string &value) const { return this->set(key.c_str(), value.c_str()); }
 
-  bool set(const char *key, const void *value, size_t size) const {
-    if (!this->handle_) {
-      return false;
-    }
-    auto err = this->handle_->set_blob(key, value, size);
-    if (err != ESP_OK) {
-      NVS_TRACE("nvs", "set blob %s failed: %s", key, esp_err_to_name(err));
-      return false;
-    }
-    NVS_TRACE("nvs", "set blob %s success: %s", key,
-              format_hex_pretty(static_cast<const uint8_t *>(value), size).c_str());
-    return true;
-  }
-
+  bool set(const char *key, const void *value, size_t size) const { return this->set_blob_(key, value, size); }
+  bool set(const std::string &key, const void *value, size_t size) const { return this->set(key.c_str(), value, size); }
   bool set(const char *key, const std::vector<uint8_t> &value) const {
     return this->set(key, value.data(), value.size());
   }
+  bool set(const std::string &key, const std::vector<uint8_t> &value) const { return this->set(key.c_str(), value); }
 
 #if __cpp_lib_span >= 202002L
   template<class T, std::size_t N> bool set(const char *key, std::span<T, N> value) const {
     return this->set(key, value.data(), value.size_bytes());
   }
+  template<class T, std::size_t N> bool set(const std::string &key, std::span<T, N> value) const {
+    return this->set(key.c_str(), value.data(), value.size_bytes());
+  }
 #endif
 
   bool reset() const { return this->handle_->erase_all() == ESP_OK; }
 
+  /// Get the blob from nvs.
+  /// if data is nullptr then returned blob size.
+  size_t get_blob(const char *key, uint8_t *data, size_t size) const;
+  size_t get_blob(const std::string &key, uint8_t *data, size_t size) const {
+    return this->get_blob(key.c_str(), data, size);
+  }
+  optional<std::vector<uint8_t>> get_blob(const char *key) const;
+  optional<std::vector<uint8_t>> get_blob(const std::string &key) const { return this->get_blob(key.c_str()); }
+
  protected:
   std::unique_ptr<nvs::NVSHandle> handle_;
+  bool set_blob_(const char *key, const void *value, size_t size) const;
+
   optional<std::string> get_string_(const char *key) const;
-  optional<std::vector<uint8_t>> get_blob_(const char *key) const;
+  bool set_string_(const char *key, const char *value) const;
+
+  optional<float> get_float_(const char *key) const;
+  bool set_float_(const char *key, float value) const;
+
+  optional<double> get_double_(const char *key) const;
+  bool set_double_(const char *key, double value) const;
+
+  optional<bool> get_bool_(const char *key) const;
+  bool set_bool_(const char *key, bool value) const;
 };
 
 }  // namespace nvs_flash
