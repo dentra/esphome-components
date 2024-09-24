10cf61d8230d5f4682ceff230bfc3bae746baa44
dentra <dennis.trachuk@gmail.com>
2024-09-24 19:50:52 +0300

    nvs: limit to esp32 platform

--- components/nvs/__init__.py
+++ components/nvs/__init__.py
@@ -1,11 +1,17 @@
 import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import PLATFORM_ESP32
 
 nvs_ns = cg.esphome_ns.namespace("nvs_flash")
 NVSFlash = nvs_ns.class_("NvsFlash")
 NVSFlashConstRef = NVSFlash.operator("ref").operator("const")
 
 
-CONFIG_SCHEMA = {}
+CONFIG_SCHEMA = cv.All(
+    cv.Schema({}),
+    cv.only_on([PLATFORM_ESP32]),
+)
+
 
 
 async def to_code(config):
