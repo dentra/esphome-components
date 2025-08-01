1d9f369963177782d1b5056665b2dc14c894822d
dentra <dennis.trachuk@gmail.com>
2025-08-01 16:00:41 +0300

    etl: update version to 20.39.4

--- components/etl/__init__.py
+++ components/etl/__init__.py
@@ -1,7 +1,7 @@
 # Dummy integration to allow relying on ETL
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.core import coroutine_with_priority
+from esphome.core import CORE, coroutine_with_priority
 
 CODEOWNERS = ["@dentra"]
 
@@ -10,4 +10,4 @@ CONFIG_SCHEMA = cv.Schema({})
 
 @coroutine_with_priority(200.0)
 async def to_code(config):
-    cg.add_library("etlcpp/Embedded Template Library", "20.35.8")
+    cg.add_library("etlcpp/Embedded Template Library", "20.39.4")
