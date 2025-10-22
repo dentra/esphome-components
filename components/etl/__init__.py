a52f1a315baf47be5a52b237216106f6ffc6bdca
dentra <dennis.trachuk@gmail.com>
2025-10-22 21:01:52 +0300

    etl: remove git version pinning

--- components/etl/__init__.py
+++ components/etl/__init__.py
@@ -11,7 +11,5 @@ CONFIG_SCHEMA = cv.Schema({})
 @coroutine_with_priority(200.0)
 async def to_code(config):
     # cg.add_library("etlcpp/Embedded Template Library", "20.39.4")
-    cg.add_library(
-        "Embedded Template Library", None, "https://github.com/ETLCPP/etl@20.39.4"
-    )
+    cg.add_library("Embedded Template Library", None, "https://github.com/ETLCPP/etl")
     cg.add_build_flag("-D PROFILE_GCC_GENERIC")
